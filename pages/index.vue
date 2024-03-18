d6b5d72348935da15196f1870eef10dfffe99bb9
Nati Namvong <nati@linecorp.com>
2024-03-18 17:21:07 +0700

    styling

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@ const copyToClipboard = (text: string) => {
     <div class="mt-4" v-for="clipboard in clipboards" :key="clipboard.id">
       <div class="flex">
         <div class="grow text-xs">{{ clipboard.name }}</div>
-        <button @click="removeClipboard(clipboard.id)" class="btn btn-xs btn-square">
+        <button @click="removeClipboard(clipboard.id)" class="btn btn-xs btn-ghost">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
