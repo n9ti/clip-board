42ec771311dbd0cbc26c27827b5db109a7268c4d
Nati Namvong <nati@linecorp.com>
2024-03-18 14:25:45 +0700

    basic setup

--- app.vue
+++ app.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 // --------------
 // Disable Pinch Zoom on Mobile Web (iOS)
 // https://stackoverflow.com/questions/11689353/disable-pinch-zoom-on-mobile-web/52201924#52201924
@@ -6,16 +6,19 @@
 document.addEventListener('gesturestart', function (e) {
   e.preventDefault();
   // special hack to prevent zoom-to-tabs gesture in safari
+  // @ts-ignore
   document.body.style.zoom = 0.99;
 });
 document.addEventListener('gesturechange', function (e) {
   e.preventDefault();
   // special hack to prevent zoom-to-tabs gesture in safari
+  // @ts-ignore
   document.body.style.zoom = 0.99;
 });
 document.addEventListener('gestureend', function (e) {
   e.preventDefault();
   // special hack to prevent zoom-to-tabs gesture in safari
+  // @ts-ignore
   document.body.style.zoom = 0.99;
 });
 </script>
