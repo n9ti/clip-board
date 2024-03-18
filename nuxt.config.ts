bc5325d1734b81db1bb6e2f144e52199e2a4c887
Nati Namvong <nati@linecorp.com>
2024-03-18 14:21:26 +0700

    basic setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      meta: [
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+        },
+        {
+          name: "apple-mobile-web-app-capable",
+          content: "yes",
+        },
+        {
+          name: "mobile-web-app-capable",
+          content: "yes",
+        },
+        {
+          name: "apple-mobile-web-app-status-bar-style",
+          content: "black-translucent",
+        },
+      ],
+    },
+  },
+  modules: [
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@vite-pwa/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+  ],
+  pinia: {
+    storesDirs: ["./stores/**"],
+  },
+  piniaPersistedstate: {
+    storage: "localStorage",
+  },
+  colorMode: {
+    preference: "retro", // default theme
+    dataValue: "theme", // activate data-theme in <html> tag
+    classSuffix: "",
+    storageKey: "nuxt-color-mode",
+  },
+  ssr: false,
+});
---
+++ pages/add.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <NuxtLink class="btn" to="/">Cancel</NuxtLink>
+</template>
